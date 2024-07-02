16f11b0cfd18c09c8d6971341abbc67241b728be
sermin asusi7 <sermin@asusi7.machine>
2024-07-02 16:09:07 +0200

    operate

--- ytapi/oauth_runnor.py
+++ ytapi/oauth_runnor.py
@@ -13,10 +13,17 @@ class OAuthRunnor(Runnor):
         super().__init__()
         print('OAuthRunnor')
         self.ACCESS_TOKEN = self.secrets['google']['token']
+        # self.cli = Client(access_token=self.ACCESS_TOKEN)
         self.ytdl: Ytdl | None = None
         self.video_file: Path | None = None
         self.title: str | None = None
         self.description: str | None = None
+        self.upload_id: str | None = None
+
+    @property
+    def cli(self):
+        cli = Client(access_token=self.ACCESS_TOKEN)
+        return cli
 
     def upload_ytdl(self, ytdl_name):
         print(f'upload_ytdl "{ytdl_name}"')
@@ -26,34 +33,42 @@ class OAuthRunnor(Runnor):
     def delete_ytdl(self, ytdl_name):
         self.init_ytdl(ytdl_name)
         # How to delete a video?
+        self.delete_video(self.upload_id)
 
     def init_ytdl(self, ytdl_name):
-        ytdl = Ytdl.post_factory(ytdl_name)
+        self.ytdl = Ytdl.post_factory(ytdl_name)
         # ytdl.print_analysis()
         # # analysis = ytdl.analysis()
         # # # print(f'analysis: {analysis}')
         # # for line in analysis:
         # #     print(f'- {line}')
         # # print()
-        self.video_file = ytdl.get_video_file()
+        self.video_file = self.ytdl.get_video_file()
         # print(f'video_file: {self.video_file}')
-        info_json = ytdl.get_fixed_json_info()
+        info_json = self.ytdl.get_fixed_json_info()
         self.title = info_json['title']
         self.description = info_json['description']
-        uploaded_json = ytdl.get_uploaded_json()
+        uploaded_json = self.ytdl.get_uploaded_json()
         print(f'uploaded_json: {uploaded_json}')
+        if uploaded_json:
+            self.upload_id = uploaded_json['id']
 
     def upload_video(self, file, title='video title', description='video description'):
-        cli = Client(access_token=self.ACCESS_TOKEN)
+        if self.upload_id:
+            self.delete_video(self.upload_id)
 
         body = mds.Video(
-            snippet=mds.VideoSnippet(title=title, description=description)
+            snippet=mds.VideoSnippet(title=title, description=description),
+            # # status=mds.VideoStatus(privacyStatus="public"),  # Set video as not private
+            status=mds.VideoStatus(privacyStatus="public", madeForKids=False)
+            # Set video as not private and not for kids
         )
 
         media = Media(filename=str(file))
 
-        upload = cli.videos.insert(
-            body=body, media=media, parts=["snippet"], notify_subscribers=True
+        upload = self.cli.videos.insert(
+            # body=body, media=media, parts=["snippet"], notify_subscribers=True
+            body=body, media=media, parts=["snippet", "status"], notify_subscribers=True
         )
 
         response = None
@@ -69,6 +84,39 @@ class OAuthRunnor(Runnor):
         # Use video class to representing the video resource.
         video = mds.Video.from_dict(response)
         print(f'Video id {video.id} was successfully uploaded.')
+        self.ytdl.write_uploaded_json(response)
+
+    def delete_video(self, upload_id):
+        pass
+        # How to delete a video? Get hints from upload_video
+
+        result = None
+        try:
+            result = self.cli.videos.delete(video_id=upload_id)
+        except Exception as e:
+            print(f'Exception: {e}')
+            # return
+        # result = self.cli.videos.delete(video_id=upload_id)
+        self.ytdl.obsolize_uploaded()
+        if result:
+            print(f'Video with id {upload_id} was successfully deleted.')
+        else:
+            print(f'Failed to delete video with id {upload_id}.')
+
+        # # response = None
+        # # status: MediaUploadProgress
+        # # response: dict
+        # # while response is None:
+        # #     print(f"Deletring video...")
+        # #     status, response = actor.
+        # #     if status is not None:
+        # #         print(f"Uploading video progress: {status.progress()}...")
+        # #
+        # # print(f'response: {response}')
+        # #
+        # #
+        # # cli.videos.delete(id=upload_id)
+        # print(f'Video with id {upload_id} was successfully deleted.')
 
 
 '''
--- ytapi_runs/upload_run_1.py
+++ ytapi_runs/upload_run_1.py
@@ -4,9 +4,25 @@ from ytapi.oauth_runnor import OAuthRunnor
 def main():
     runnor = OAuthRunnor()
     # runnor.show_default_channel_info()
-    # runnor.upload_ytdl('beating')
-    runnor.delete_ytdl('beating')
+    # name = 'beating'
+    name = 'flyswamp'
+    # name = 'nature_forest'
+    runnor.upload_ytdl(name)
+    # runnor.delete_ytdl('beating')
 
 
 if __name__ == "__main__":
     main()
+
+
+#1  gisselfeld_fireworks           Gisselfelds fyrværkeri, der bortjager tusindvis af fugle.
+#2  beating                        Slåning af Søtorup-enge.avi
+#3  feb2011                        feb2011.AVI
+#4  nielstrup1970                  Nielstrup Sø cirka 1970
+#5  flyswamp                       Filmoptagelse fra fly over Holmegaard Mose
+#6  nature_forest                  Sjælden filmoptagelse af forsvunden naturskov
+#7  mouse_awake                    Musvåge fanget i fælde sættes fri
+#8  Gisselfeld_abuse_1             Gisselfelds mishandlede landbrugsjorder (video nr.1)
+#9  Gisselfeld_abuse_2             Gisselfelds mishandlede landbrugsjorder (video nr. 2)
+#10 Gisselfeld_ratproblem          Gisselfelds jagtforretning skaber rotteproblemer
+#11 Gisselfeld_destroy_deep_lake   Gisselfeld ødelægger Sjællands næstdybeste sø
